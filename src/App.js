d33653b101c5bf8a4f031fe59519bd4ec3fdc6d6
Milad_Mrk <milad.rastgoou@gmail.com>
2022-06-18 13:21:30 +0430

    text ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Product from "./Components/Product";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="bg-slate-200  pt-11">
+      <header>
+        {/* AppBar - Header */}
+        <div className=" flex flex-row-reverse justify-between items-center  px-4 text-slate-800">
+          <div className="flex justify-center items-center bg-white shadow-lg rounded-[5px]  ">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-7 w-7 m-[2px]"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              />
+            </svg>
+          </div>
+          <div className="font-bold">ساعت هوشمند</div>
+          <div>LOGO</div>
+        </div>
+
+        {/* Filter section */}
+        <section className="flex px-4 gap-2 mt-6">
+          <div className="flex items-center justify-evenly  bg-white  rounded-md w-1/2 p-3 shadow-sm">
+            <div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6 text-orange-600"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3 4h13M3 8h9m-9 4h9m5-4v12m0 0l-4-4m4 4l4-4"
+                />
+              </svg>
+            </div>
+            <div className=" text-sm font-medium  ">محبوب ترین</div>
+          </div>
+          <div className="flex items-center justify-evenly  bg-white  rounded-md  w-1/2 p-3 shadow-sm">
+            <div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3 4a1 1 0 011-1h16a1 1 0 011 1v2.586a1 1 0 01-.293.707l-6.414 6.414a1 1 0 00-.293.707V17l-4 4v-6.586a1 1 0 00-.293-.707L3.293 7.293A1 1 0 013 6.586V4z"
+                />
+              </svg>
+            </div>
+            <div className="text-sm font-medium ">فیلتر: اپل</div>
+          </div>
+        </section>
       </header>
+      {/* Products */}
+      <section className="mt-6 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 px-4">
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+      </section>
     </div>
   );
 }
---
+++ src/Components/Product.js
@@ -0,0 +1,87 @@
+import React, { useState } from "react";
+const Product = () => {
+  const [color, setColor] = useState("red");
+  return (
+    <div className="bg-white rounded-md shadow-md p-2">
+      <div className="bg-slate-300 rounded-md px-6 flex justify-center items-center">
+        <img src={require("../../src/assets/img/Cart/1.png")} alt="product" />
+      </div>
+      <div className=" flex justify-between items-center mt-3">
+        <span className=" text-sm md:text-base text-slate-400">اپل</span>
+        <div className="flex items-center">
+          <div
+            onClick={() => setColor("red")}
+            className="w-5 h-5  md:h-8 md:w-8 rounded-full bg-red-400 border-2 border-gray-50 -ml-1.5 flex justify-center items-center"
+          >
+            {color === "red" && (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-3 w-3 md:h-7 md:w-7 font-bold text-white stroke-orange-50 ring-2 ring-red-400 rounded-full ring-offset-1"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            )}
+          </div>
+          <div
+            onClick={() => setColor("blue")}
+            className="w-5 h-5  md:h-8 md:w-8 rounded-full bg-blue-400 border-2 border-gray-50 -ml-1.5 flex justify-center items-center"
+          >
+            {" "}
+            {color === "blue" && (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-3 w-3 md:h-7 md:w-7 font-bold text-white stroke-orange-50 ring-2 ring-blue-400 rounded-full ring-offset-1"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            )}
+          </div>
+          <div
+            onClick={() => setColor("green")}
+            className="w-5 h-5  md:h-8 md:w-8 rounded-full bg-green-400 border-2 border-gray-50 flex justify-center items-center"
+          >
+            {" "}
+            {color === "green" && (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-3 w-3 md:h-7 md:w-7 font-bold text-white stroke-orange-50  ring-2 ring-green-400 rounded-full ring-offset-1"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            )}
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col text-xs items-center mt-3 font-medium text-slate-800 md:text-base">
+        <span>ساعت هوشمند اپل سری6</span>
+      </div>
+      <div className="flex flex-col text-xs items-center mt-3 font-medium text-orange-600 md:text-base">
+        <span> 48.3210.000 تومان</span>
+      </div>
+      <hr className="mt-2" />
+      <button className="text-sm py-1   mt-2 w-full font-bold text-orange-600 md:text-base ">
+        مشاهده و سفارش
+      </button>
+    </div>
+  );
+};
+
+export default Product;
